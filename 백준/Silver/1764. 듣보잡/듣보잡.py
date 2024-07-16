dff4d0d60d6037a6066460596585dba2bf6f6536
JeongHoon Woo <frndhoon@gmail.com>
2024-07-16 11:17:42 +0900

    [Silver IV] Title: 듣보잡, Time: 104 ms, Memory: 43924 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+listen_num, see_num = map(int, input().split())
+
+listen_arr = set([])
+see_arr = set([])
+listen_see_arr = set([])
+
+for _ in range(listen_num):
+    man = input().rstrip()
+    listen_arr.add(man)
+
+for _ in range(see_num):
+    man = input().rstrip()
+    see_arr.add(man)
+
+for people in listen_arr:
+    if people in see_arr:
+        listen_see_arr.add(people)
+
+listen_see_arr = sorted(listen_see_arr)
+
+print(len(listen_see_arr))
+print(*listen_see_arr, sep="\n")
