f611a80f1401fa508a48d37b44b013f1a7a78344
JeongHoon Woo <frndhoon@gmail.com>
2024-07-16 09:25:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 252 ms, Memory: 57404 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+pocket_num, q_num = map(int, input().split())
+
+pocketmon_dic = {}
+reverse_pocketmon_dic = {}
+
+for idx in range(1, pocket_num + 1):
+    name = input().strip()
+    pocketmon_dic[idx] = name
+    reverse_pocketmon_dic[name] = str(idx)
+
+for _ in range(q_num):
+    question = input().strip()
+    if question[0].isdigit():
+        print(pocketmon_dic[int(question)])
+    else:
+        print(reverse_pocketmon_dic[question])
