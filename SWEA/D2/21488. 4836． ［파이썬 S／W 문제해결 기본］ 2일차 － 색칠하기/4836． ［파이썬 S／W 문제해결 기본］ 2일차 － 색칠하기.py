1657a0db494c475b1418974fb52c26c7970f16ce
JeongHoon Woo <frndhoon@gmail.com>
2024-07-31 22:56:09 +0900

    [D2] Title: 4836. [파이썬 S/W 문제해결 기본] 2일차 - 색칠하기, Time: 102 ms, Memory: 45,056 KB -BaekjoonHub

--- SWEA/D2/21488. 4836． ［파이썬 S／W 문제해결 기본］ 2일차 － 색칠하기/4836． ［파이썬 S／W 문제해결 기본］ 2일차 － 색칠하기.py
+++ SWEA/D2/21488. 4836． ［파이썬 S／W 문제해결 기본］ 2일차 － 색칠하기/4836． ［파이썬 S／W 문제해결 기본］ 2일차 － 색칠하기.py
@@ -1,7 +1,6 @@
 T = int(input())
 
-
-for testcase in range(1, T+1):
+for testcase in range(1, T + 1):
     # 빨강, 파랑 칠할 list 저장
     grid = [[[0, 0] for _ in range(10)] for _ in range(10)]
     n = int(input())
@@ -11,11 +10,14 @@ for testcase in range(1, T+1):
         r1, c1, r2, c2, color = map(int, input().split())
         for x in range(r1, r2 + 1):
             for y in range(c1, c2 + 1):
+                # 둘 다 칠해져있으면 건너뛰기
+                if grid[x][y][0] == 1 and grid[x][y][1] == 1:
+                    continue
                 grid[x][y][color - 1] += 1
 
     for x in range(10):
         for y in range(10):
-            # 둘 다 칠해져있으면
+            # 둘 다 칠해져있을 때만
             if grid[x][y] == [1, 1]:
                 purple += 1
 
