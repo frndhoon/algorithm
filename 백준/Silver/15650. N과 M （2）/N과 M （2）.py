74a30f8a51fcb82458ce952de67b413b7bb5d3ba
JeongHoon Woo <frndhoon@gmail.com>
2024-08-02 14:19:51 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,22 +1,19 @@
 n, m = map(int, input().split())
 
+result = []
 
-def back_tracking(result):
+
+def back_tracking(idx):
 
     if len(result) == m:
         print(*result)
+        return
 
-    for num in range(1, n + 1):
+    for num in range(idx, n + 1):
         if num not in result:
-            if result: # 현재 검사할 list가 있다면,
-                if num > result[-1]: # 전 원소랑 비교하기
-                    result.append(num)
-                    back_tracking(result)
-                    result.pop()
-            else:
-                result.append(num)
-                back_tracking(result)
-                result.pop()
+            result.append(num)
+            back_tracking(num + 1)
+            result.pop()
 
 
-back_tracking([])
+back_tracking(1)
