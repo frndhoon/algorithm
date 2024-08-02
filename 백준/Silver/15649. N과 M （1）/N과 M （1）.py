78e20b3aae208a5b98ee635535bc0d5324c299da
JeongHoon Woo <frndhoon@gmail.com>
2024-08-02 12:11:28 +0900

    [Silver III] Title: N과 M (1), Time: 204 ms, Memory: 115188 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,5 +1,4 @@
 n, m = map(int, input().split())
-visited = [False] * (n + 1)
 
 
 def back_tracking(result):
@@ -8,12 +7,10 @@ def back_tracking(result):
         return
 
     for num in range(1, n+1):
-        if not visited[num]:
-            visited[num] = True
+        if num not in result:
             result.append(num)
             back_tracking(result)
             result.pop()
-            visited[num] = False
 
 
 back_tracking([])
