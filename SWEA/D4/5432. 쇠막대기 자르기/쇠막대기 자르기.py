9ff853b22a45053763c6f52323f40c066001fc1d
JeongHoon Woo <frndhoon@gmail.com>
2024-08-04 22:55:19 +0900

    [D4] Title: 쇠막대기 자르기, Time: 283 ms, Memory: 80,512 KB -BaekjoonHub

---
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for testcase in range(1, T+1):
+    sliver_rod = list(input())
+    cnt = 0
+    stack = []
+
+    for idx in range(len(sliver_rod)):
+        # 열렸으면 막대 만들기
+        if sliver_rod[idx] == '(':
+            stack.append(sliver_rod[idx])
+
+        # 닫혔으면 레이저 or 막대 끝
+        elif sliver_rod[idx] == ')':
+            stack.pop()
+            recent_rod = sliver_rod[idx - 1]
+
+            # 연속됐으면 레이저로 열린 막대 수만큼 카운트
+            if recent_rod == '(':
+                cnt += len(stack)
+            # 아니라면 막대 마지막 조각 카운트
+            elif recent_rod == ')':
+                cnt += 1
+
+    print(f'#{testcase} {cnt}')
+
