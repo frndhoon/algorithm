9cc9f34641bb19de88884f4f1b62f5486a771df3
JeongHoon Woo <frndhoon@gmail.com>
2024-08-04 23:04:41 +0900

    [D4] Title: 쇠막대기 자르기, Time: 264 ms, Memory: 86,432 KB -BaekjoonHub

--- SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -2,25 +2,23 @@ T = int(input())
 
 for testcase in range(1, T+1):
     sliver_rod = list(input())
+    rod = 0
     cnt = 0
-    stack = []
 
     for idx in range(len(sliver_rod)):
         # 열렸으면 막대 만들기
         if sliver_rod[idx] == '(':
-            stack.append(sliver_rod[idx])
-
+            rod += 1
+        
         # 닫혔으면 레이저 or 막대 끝
         elif sliver_rod[idx] == ')':
-            stack.pop()
-            recent_rod = sliver_rod[idx - 1]
-
-            # 연속됐으면 레이저로 열린 막대 수만큼 카운트
-            if recent_rod == '(':
-                cnt += len(stack)
+            # 연속됐으면 레이저로 열린 막대 수만큼 자르기
+            if sliver_rod[idx - 1] == '(':
+                rod -= 1
+                cnt += rod
             # 아니라면 막대 마지막 조각 카운트
-            elif recent_rod == ')':
+            else:
+                rod -= 1
                 cnt += 1
 
     print(f'#{testcase} {cnt}')
-
