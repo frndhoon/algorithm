973852930a343758b32cd887f9a4b8fcc4ff1ec5
JeongHoon Woo <frndhoon@gmail.com>
2024-09-02 15:05:13 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 195 ms, Memory: 49,876 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,34 +1,32 @@
-T = int(input())
-
-def back_tracking(k=0):
+# [1] 교환 횟수만큼 교환했을 때, 최대값 비교
+def solve(cnt, num_list):
     global max_num
 
-    if k == cnt:
-        max_num = max(max_num, int(''.join(list_num)))
+    if cnt == int(change_cnt):
+        max_num = max(max_num, int(''.join(num_list)))
         return
 
-    for i in range(N - 1):
-        for j in range(i+1, N):
-            list_num[i], list_num[j] = list_num[j], list_num[i]
-            current_num = int(''.join(list_num))
+    for i in range(len(num) - 1):
+        for j in range(i+1, len(num)):
+            num_list[i], num_list[j] = num_list[j], num_list[i]
+            current_num = ''.join(num_list)
 
-            if (k, current_num) not in duplicate:
-                back_tracking(k + 1)
-                duplicate.add((k, current_num))
+            if (cnt, current_num) not in visited:  # [2] 교환했을 때, 이미 비교된 값이라면 비교하지말기
+                visited.add((cnt, current_num))
+                solve(cnt+1, num_list)
 
-            list_num[i], list_num[j] = list_num[j], list_num[i]
+            num_list[i], num_list[j] = num_list[j], num_list[i]
+
+T = int(input())
 
 for testcase in range(1, T+1):
-    num, cnt = map(int, input().split())
-    list_num = list(str(num))
-    N = len(list_num)
     max_num = 0
 
-    duplicate = set()
+    num, change_cnt = input().split()
+    num_list = list(num)
 
-    # cnt가 0이거나 모든 숫자가 같은 경우를 처리
-    if cnt == 0 or len(set(list_num)) == 1:
-        print(f'#{testcase} {num}')
-    else:
-        back_tracking()
-        print(f'#{testcase} {max_num}')
+    visited = set()  # (횟수, 숫자)
+
+    solve(0, num_list)
+
+    print(f'#{testcase} {max_num}')
