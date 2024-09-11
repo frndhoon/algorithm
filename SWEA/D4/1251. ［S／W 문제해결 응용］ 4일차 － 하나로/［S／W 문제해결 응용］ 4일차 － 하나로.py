bc1810a6a277257d24b3103093707d1dec480f33
JeongHoon Woo <frndhoon@gmail.com>
2024-09-11 16:33:32 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 8,459 ms, Memory: 125,816 KB -BaekjoonHub

---
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.py
@@ -0,0 +1,47 @@
+import heapq
+T = int(input())
+
+
+def find(x):
+    if parents[x] != x:
+        parents[x] = find(parents[x])
+    return parents[x]
+
+
+def union(a, b):
+    rootA = find(a)
+    rootB = find(b)
+
+    if rootA != rootB:
+        if rootA < rootB:
+            parents[rootB] = rootA
+        else:
+            parents[rootA] = rootB
+
+
+for testcase in range(1, T+1):
+    island_cnt = int(input())
+    x_list = list(map(int, input().split()))
+    y_list = list(map(int, input().split()))
+
+    pq = []
+
+    for i in range(island_cnt-1):
+        for j in range(i+1, island_cnt):
+            distance = abs(x_list[i] - x_list[j]) ** 2 + abs(y_list[i] - y_list[j]) ** 2
+            heapq.heappush(pq, (distance, i, j))
+
+    parents = [i for i in range(island_cnt)]
+
+    # 환경 부담 세율
+    E = float(input())
+    min_dist = 0
+
+    while pq:
+        dist, start, end = heapq.heappop(pq)
+
+        if find(start) != find(end):
+            min_dist += dist
+            union(start, end)
+
+    print(f'#{testcase} {round(min_dist*E)}')
