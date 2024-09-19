84de9d075f573fcc2873d2337b1655a9ddf980f5
JeongHoon Woo <frndhoon@gmail.com>
2024-09-19 09:30:44 +0900

    [Gold V] Title: 치킨 배달, Time: 184 ms, Memory: 113636 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -21,12 +21,12 @@ min_dist = 1e9
 def get_chicken_distance():
     entire_distance = 0
 
-    for i in homes:  # 집 좌표에 대해 모든 치킨집과의 거리를 구함
+    for i in homes:  # 집 좌표에 대해 모든 치킨집과의 거리 구함
         distance = 1e9
         for j in range(len(visited)):
             if visited[j]:
                 check_num = abs(i[0] - chicks[j][0]) + abs(i[1] - chicks[j][1])
-                distance = min(distance, check_num)  # 각 집에 대해 치킨 거리가 최소인 값을 구함
+                distance = min(distance, check_num)  # 각 집에 대해 치킨집 거리가 최솟값 구함
 
         entire_distance += distance
 
