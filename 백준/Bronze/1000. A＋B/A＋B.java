d8091ed10f05637d90f1e6a87280e53a9db22078
JeongHoon Woo <frndhoon@gmail.com>
2024-10-01 09:12:06 +0900

    [Bronze V] Title: A+B, Time: 168 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt(), b = input.nextInt();
+        System.out.print(a+b);
+    }
+}
