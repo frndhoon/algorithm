d769cb8fd0ae0e4cb17b5e9c779631c2fb15ea96
JeongHoon Woo <frndhoon@gmail.com>
2024-10-07 08:01:57 +0900

    [D3] Title: 나무 베기, Time: 219 ms, Memory: 52,316 KB -BaekjoonHub

--- SWEA/D3/22683. 나무 베기/나무 베기.py
+++ SWEA/D3/22683. 나무 베기/나무 베기.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 GROUND_MOVE = 'G'
 TREE_CANT_MOVE = 'T'
@@ -30,24 +30,25 @@ for testcase in range(1, T + 1):
     distances = [[[[float('inf')] * (cut_cnt + 1) for _ in range(4)] for _ in range(N)] for _ in range(N)]
     distances[start_col][start_row][0][cut_cnt] = 0  # 시작점에서의 리모컨 조작 횟수는 0, 방향은 'up'
 
-    def bfs(start_col, start_row, cut_cnt):
+    def dijkstra(start_col, start_row, cut_cnt):
         """
-        bfs 탐색
+        다익스트라 탐색
         :param start_col: 시작 지점(행)
         :param start_row: 시작 지점(열)
         :param cut_cnt: 최대 나무 베기 개수
         :return:
         """
+        # 우선순위 큐를 사용하여 최소 비용 경로 탐색
         # 열, 행, 방향, 남은 cut_cnt, 리모컨 조작 횟수
-        queue = deque([(start_col, start_row, 0, cut_cnt, 0)])
+        heap = [(0, start_col, start_row, 0, cut_cnt)]  # 리모컨 조작 횟수, 현재 열, 행, 방향, 남은 cut_cnt
 
         # 상, 우, 하, 좌 방향
         DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-        while queue:
-            current_col, current_row, current_view, remaining_cut_cnt, remote_cnt = queue.popleft()
+        while heap:
+            remote_cnt, current_col, current_row, current_view, remaining_cut_cnt = heapq.heappop(heap)
 
-            # 도착 했으면 탐색 종료
+            # 현재 위치가 도착 지점일 때
             if matrix[current_col][current_row] == ARRIVE_LOCATION: continue
 
             # 현재 방향으로 앞으로 이동 시도
@@ -59,23 +60,22 @@ for testcase in range(1, T + 1):
                 if matrix[next_col][next_row] == GROUND_MOVE or matrix[next_col][next_row] == ARRIVE_LOCATION:
                     if distances[next_col][next_row][current_view][remaining_cut_cnt] > remote_cnt + 1:
                         distances[next_col][next_row][current_view][remaining_cut_cnt] = remote_cnt + 1
-                        queue.append((next_col, next_row, current_view, remaining_cut_cnt, remote_cnt + 1))
+                        heapq.heappush(heap, (remote_cnt + 1, next_col, next_row, current_view, remaining_cut_cnt))
 
                 # TREE_CANT_MOVE로 이동하려면 나무를 베어야 함
                 elif matrix[next_col][next_row] == TREE_CANT_MOVE and remaining_cut_cnt > 0:
                     if distances[next_col][next_row][current_view][remaining_cut_cnt - 1] > remote_cnt + 1:
                         distances[next_col][next_row][current_view][remaining_cut_cnt - 1] = remote_cnt + 1
-                        queue.append((next_col, next_row, current_view, remaining_cut_cnt - 1, remote_cnt + 1))
+                        heapq.heappush(heap, (remote_cnt + 1, next_col, next_row, current_view, remaining_cut_cnt - 1))
 
             # 좌우 회전 (방향만 바뀌고 이동은 하지 않음)
             new_views = [(current_view - 1) % 4, (current_view + 1) % 4]  # 좌회전, 우회전
             for new_view in new_views:
                 if distances[current_col][current_row][new_view][remaining_cut_cnt] > remote_cnt + 1:
                     distances[current_col][current_row][new_view][remaining_cut_cnt] = remote_cnt + 1
-                    queue.append((current_col, current_row, new_view, remaining_cut_cnt, remote_cnt + 1))
+                    heapq.heappush(heap, (remote_cnt + 1, current_col, current_row, new_view, remaining_cut_cnt))
 
-
-    bfs(start_col, start_row, cut_cnt)
+    dijkstra(start_col, start_row, cut_cnt)
 
     # 도착지점의 최소 리모컨 조작 횟수 확인
     arrive_col, arrive_row = get_point(ARRIVE_LOCATION)
