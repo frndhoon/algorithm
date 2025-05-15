f712b7f5be163db016471adc32a1c2a9b1c7e3a9
JeongHoon Woo <frndhoon@gmail.com>
2025-05-15 09:37:24 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String st = String.valueOf(n);
+        for (int i = 0; i < st.length(); i++) {
+            answer += st.charAt(i) - '0';
+        }
+        return answer;
+    }
+}
